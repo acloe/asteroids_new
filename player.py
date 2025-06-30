fb4831d9322240cca65fe98ab6961af37439aee6
acloe <acloe1@gmail.com>
2025-06-30 12:04:16 -0700

    Player shoots at a reduced speed

--- player.py
+++ player.py
@@ -1,10 +1,12 @@
 from circleshape import *
 from constants import *
+from shot import *
 
 class Player(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -23,7 +25,13 @@ class Player(CircleShape):
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
-        
+    
+    def shoot(self, x, y):
+        if self.timer <= 0:
+            bullet = Shot(x, y, SHOT_RADIUS)
+            bullet.velocity = (pygame.Vector2(0, 1).rotate(self.rotation)) * PLAYER_SHOOT_SPEED
+            self.timer = PLAYER_SHOOT_COOLDOWN
+    
     def update(self, dt):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
@@ -34,4 +42,7 @@ class Player(CircleShape):
                 self.move(dt)
         if keys[pygame.K_s]:
                 self.move(-dt)
+        if keys[pygame.K_SPACE]:
+                self.shoot(self.position.x, self.position.y)
+        self.timer -= dt
 
